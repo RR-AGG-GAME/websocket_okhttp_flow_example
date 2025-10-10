c4db89810a6d22bf301fc6078e969494a8087eb6
Suraj Alta <suraj.alta@ecashpay.com.ph>
2025-10-10 07:07:13 +0530

    binary removed

--- app/src/main/java/com/example/websocketflow/WebSocketMessage.kt
+++ app/src/main/java/com/example/websocketflow/WebSocketMessage.kt
@@ -2,6 +2,5 @@ package com.example.websocketflow
 
 sealed class WebSocketMessage {
     data class Text(val text: String) : WebSocketMessage()
-    data class Binary(val bytes: ByteArray, val contentType: String? = null) : WebSocketMessage()
 }
 
